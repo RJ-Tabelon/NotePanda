27d4a85ae49ed1e1af362e62f8eb8f5ae42d5629
RJ Tabelon <tabelonrj@gmail.com>
2025-08-24 15:02:06 -0400

    Prepare for deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'http://localhost:5001/api' });
+// no localhost in production, so make this dynamic
+const BASE_URL = import.meta.env.MODE === "development" ? 'http://localhost:5001/api' : "/api"
+
+const api = axios.create({ BASE_URL });
 
 export default api;
